0b4d3e2d908367ab1e294927ed62b789a1874491
Github ID <test@test.com>
2022-06-24 11:08:59 +0900

    06.24

--- trn01/js/main.js
+++ trn01/js/main.js
@@ -93,6 +93,12 @@ $(function(){
             $('.toTop').fadeOut(1000)
 
         }
+    });
+
+    $('#fl').on('change',function(){
+        console.log($(this),$(this).val())
+        var lnk = $(this).val();
+        if(lnk) window.open(lnk);
     })
 
 
