388e33eb1fe758bdbcaa907eaf64b17eab4a4525
Github ID <test@test.com>
2022-06-29 12:20:23 +0900

    06.29

--- 시안코딩/js/index.js
+++ 시안코딩/js/index.js
@@ -7,4 +7,20 @@ $(function(){
         pauseOnFocus:false,
 
     });
+
+    $('.mainboxes').slick({
+        arrows:false, 
+        dots: true,
+        slidesToShow:3,
+        centerMode: true,
+
+    });
+    $(' .productArrows i:nth-child(1)').on('click', function(){
+        $('.mainboxes').slick('slickPrev')
+    })
+    $(' .productArrows i:nth-child(2)').on('click', function(){
+        $('.mainboxes').slick('slickNext')
+    });
+
+
 })
