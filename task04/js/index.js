04b6e226d5ce969c6e5460ab527675d545a959d7
Github ID <test@test.com>
2022-07-22 15:44:30 +0900

    07.22

--- task04/js/index.js
+++ task04/js/index.js
@@ -2,6 +2,36 @@ $(function(){
     $('.hamburger').on('click',function(){
         $(this).toggleClass('is-active');
         $('.m_smenu').toggleClass('on')
-    })
+    });
+
+   $('.Vs_container').slick({
+        arrows: false,
+        autoplay: false,
+        pasueOnHover: false,
+        pasueOnFocus: false,
+        autoplaySpeed:4000,
+        dots:true,
+        Responsive:[
+            {
+                breakpoint:1200,
+                settings:{
+                    autoplay:true,
+                    autoplaySpeed:4000,
+                }
+            }
+        ]
+    
+
+   });
+
+
+   $('.slider_icon i:nth-child(1)').on('click', function(){
+    $('.Vs_container').slick('slickPrev');
 })
+$('.slider_icon i:nth-child(2)').on('click', function(){
+    $('.Vs_container').slick('slickNext');
+})
+
+})
+
 
