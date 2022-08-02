12faa98c5be6dec34817b45365c7f5ba1475a033
Github ID <test@test.com>
2022-08-02 16:57:42 +0900

    08.02

--- task05/js/index.js
+++ task05/js/index.js
@@ -6,17 +6,7 @@ $(function(){
         pasueOnFocus: false,
         autoplaySpeed:3000,
     });
-    $("#bgndVideo").YTPlayer({
-        videoURL:'https://youtu.be/0qjGn_AunuA',
-        containment:'.diviceContent',
-        autoPlay:true,
-        mute:true,
-        showControls: false,
-        playOnlyIfVisible: true,
-        useOnMobile: true,
-        stopMovieOnBlur: false,
-       
-    });
+    
       
 })
 
