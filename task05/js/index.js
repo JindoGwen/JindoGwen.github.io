69b4f2e043feb5883c7a2f6ec4d11218591c1f45
Github ID <test@test.com>
2022-08-02 09:08:35 +0900

    08.02

--- task05/js/index.js
+++ task05/js/index.js
@@ -4,7 +4,19 @@ $(function(){
         autoplay: true,
         pasueOnHover: false,
         pasueOnFocus: false,
-        autoplaySpeed:4000,
+        autoplaySpeed:3000,
     });
+    $("#bgndVideo").YTPlayer({
+        videoURL:'https://youtu.be/0qjGn_AunuA',
+        containment:'.diviceContent',
+        autoPlay:true,
+        mute:true,
+        showControls: false,
+        playOnlyIfVisible: true,
+        useOnMobile: true,
+        stopMovieOnBlur: false,
+       
+    });
+      
 })
 
