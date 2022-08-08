14f8520d6dd2a82fa698ffab61557d61739e8ac4
Github ID <test@test.com>
2022-08-08 17:47:47 +0900

    08.08

---
+++ task06/js/index.js
@@ -0,0 +1,17 @@
+$("#bgndVideo").YTPlayer({
+    videoURL:'https://youtu.be/y69zckiSygU',
+    containment:'.mainVideo',
+    autoPlay:true,
+    mute:true,
+    showControls: false,
+    playOnlyIfVisible: true,
+    useOnMobile: true,
+    stopMovieOnBlur: false,
+   
+});
+ 
+
+
+
+
+
