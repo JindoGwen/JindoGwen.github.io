37af9d2698fad2f9b7db5c17515c933ec352dad6
Github ID <test@test.com>
2022-08-17 17:45:37 +0900

    08.17

--- task06/js/index.js
+++ task06/js/index.js
@@ -3,3 +3,38 @@ $('.hamburger').on('click',function(){
     $('.gnb').toggleClass('on')
 });
 
+
+
+$("#bgndVideo").YTPlayer({
+    videoURL:'https://youtu.be/yoERVK7ifK8',
+    containment:'.leftContent',
+    autoPlay:true,
+    mute:true,
+    showControls: false,
+    playOnlyIfVisible: true,
+    useOnMobile: true,
+    stopMovieOnBlur: false,
+   
+});
+
+
+
+
+$('.storeBox').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 8000,
+    arrows:false,
+    dots: true,
+  });
+
+ $(' .boxArrows i:nth-child(1)').on('click' , function (){
+    $('.storeBox').slick('slickPrev')
+    
+});
+$(' .boxArrows i:nth-child(2)').on('click' , function (){
+    $('.storeBox').slick('slickNext')
+    
+});
+
